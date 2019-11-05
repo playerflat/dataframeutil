e4e05de2eba392f7d962a3cabb6966e7783cc710
playerflat <dlguswn77771@naver.com>
2019-11-05 15:42:38 +0900

    init

---
+++ dataframeUtil.go
@@ -0,0 +1,31 @@
+package resources
+
+import (
+	"io/ioutil"
+	"strings"
+
+	"github.com/kniren/gota/dataframe"
+)
+
+// OpenCSV csv 간단하게 불러오기
+func OpenCSV(location string, Header bool, Delimiter rune) dataframe.DataFrame {
+	content, _ := ioutil.ReadFile(location)
+	ioContent := strings.NewReader(string(content))
+
+	csv := dataframe.ReadCSV(ioContent,
+		dataframe.WithDelimiter(Delimiter),
+		dataframe.HasHeader(Header))
+
+	return csv
+}
+
+// ReplaceNaN 데이터프레임의 NaN값을 원하는 값으로 변경
+func ReplaceNaN(df dataframe.DataFrame, NaNwords string, Newwords string) {
+	for i := 0; i < df.Nrow(); i++ {
+		for j := 0; j < df.Ncol(); j++ {
+			if df.Elem(i, j).String() == NaNwords {
+				df.Elem(i, j).Set(Newwords)
+			}
+		}
+	}
+}
