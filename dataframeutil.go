56335564391d42de7d35a69a848353e74661c502
playerflat <dlguswn77771@naver.com>
2019-11-06 16:25:08 +0900

    Add CheckError, Querytodf

--- dataframeutil.go
+++ dataframeutil.go
@@ -1,41 +1,111 @@
-package dataframeutil
+package main
 
 import (
+	"database/sql"
 	"io/ioutil"
+	"log"
 	"strings"
 
+	_ "github.com/go-sql-driver/mysql"
 	"github.com/kniren/gota/dataframe"
-	"github.com/kniren/gota/series"
 )
 
-// DataFrame gota의 dataframe.DataFrame
-type DataFrame struct {
-	columns []series.Series
-	ncols   int
-	nrows   int
-	Err     error
+// 에러 검사
+func CheckError(err error) {
+	if err != nil {
+		panic(err)
+	}
 }
 
-// OpenCSV csv 간단하게 불러오기
-func OpenCSV(location string, Header bool, Delimiter rune) dataframe.DataFrame {
+// OpenCSV csv 파일을 데이터프레임으로 반환 (파일위치 string, 컬럼 헤더 여부 bool, 구분자 rune) 리턴타입 DataFrame
+func OpenCSV(location string, isColHeader bool, delimiter rune) dataframe.DataFrame {
 	content, _ := ioutil.ReadFile(location)
 	ioContent := strings.NewReader(string(content))
 
 	csv := dataframe.ReadCSV(ioContent,
-		dataframe.WithDelimiter(Delimiter),
-		dataframe.HasHeader(Header))
+		dataframe.WithDelimiter(delimiter),
+		dataframe.HasHeader(isColHeader))
 
 	return csv
 }
 
-// ReplaceNaN 데이터프레임의 NaN값을 원하는 값으로 변경
-func ReplaceNaN(df dataframe.DataFrame, NaNwords string, Newwords string) dataframe.DataFrame {
+// ReplaceElem 데이터프레임의 특정 값들을 원하는 값으로 변경(데이터프레임 df, 이전문자 string, 바꿀문자 string) 리턴타입 DataFrame
+func ReplaceElem(df dataframe.DataFrame, oldstring string, newstring string) dataframe.DataFrame {
 	for i := 0; i < df.Nrow(); i++ {
 		for j := 0; j < df.Ncol(); j++ {
-			if df.Elem(i, j).String() == NaNwords {
-				df.Elem(i, j).Set(Newwords)
+			if df.Elem(i, j).String() == oldstring {
+				df.Elem(i, j).Set(newstring)
 			}
 		}
 	}
 	return df
+}
+
+// Querytodf mysql DB에 쿼리를 날려 받아온 정보를 데이터프레임으로 반환(쿼리 string, DB 접속정보 `계정명:비밀번호@연결형식(ip:port)/DB이름`)
+func Querytodf(q string, DBinfo string) dataframe.DataFrame {
+	// Open database connection
+	db, err := sql.Open("mysql", DBinfo)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
+
+	// Execute the query
+	rows, err := db.Query(q)
+	CheckError(err)
+
+	// Get column names
+	columns, err := rows.Columns()
+	CheckError(err)
+
+	var template string
+
+	for i := 0; i < len(columns); i++ {
+		if i+1 != len(columns) {
+			template += columns[i] + ","
+		} else {
+			template += columns[i] + "\n"
+		}
+	}
+
+	// Make a slice for the values
+	values := make([]sql.RawBytes, len(columns))
+
+	// rows.Scan wants '[]interface{}' as an argument, so we must copy the
+	// references into such a slice
+	// See http://code.google.com/p/go-wiki/wiki/InterfaceSlice for details
+	scanArgs := make([]interface{}, len(values))
+	for i := range values {
+		scanArgs[i] = &values[i]
+	}
+
+	// Fetch rows
+	for rows.Next() {
+		// get RawBytes from data
+		err = rows.Scan(scanArgs...)
+		CheckError(err)
+
+		// Now do something with the data.
+		// Here we just print each column as a string.
+		var value string
+		for i, col := range values {
+			// Here we can check if the value is nil (NULL value)
+			if col == nil {
+				value = "NULL"
+			} else {
+				value = string(col)
+			}
+			if i != 3 {
+				template += value + ","
+			} else {
+				template += value + "\n"
+			}
+		}
+	}
+	err = rows.Err()
+	CheckError(err)
+
+	queryResult := dataframe.ReadCSV(strings.NewReader(template))
+
+	return queryResult
 }
--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/playerflat/dataframeutil
 go 1.13
 
 require (
+	github.com/go-sql-driver/mysql v1.4.1
 	github.com/gonum/blas v0.0.0-20181208220705-f22b278b28ac // indirect
 	github.com/gonum/floats v0.0.0-20181209220543-c233463c7e82 // indirect
 	github.com/gonum/integrate v0.0.0-20181209220457-a422b5c0fdf2 // indirect
@@ -11,4 +12,5 @@ require (
 	github.com/gonum/matrix v0.0.0-20181209220409-c518dec07be9 // indirect
 	github.com/gonum/stat v0.0.0-20181125101827-41a0da705a5b // indirect
 	github.com/kniren/gota v0.9.0
+	google.golang.org/appengine v1.6.5 // indirect
 )
