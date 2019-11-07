ca2ad746e33f533ba8a558696ecf7034fc6f4649
playerflat <dlguswn77771@naver.com>
2019-11-07 17:10:53 +0900

    edit comment

--- dataframeutil.go
+++ dataframeutil.go
@@ -9,13 +9,13 @@ import (
 	"github.com/kniren/gota/dataframe"
 )
 
-var (
+var(
 	db *sql.DB
 )
 
-// Open 드라이버네임 string, DB 접속정보 string, maxopenconns int, maxidleconns int
+// Open 데이터베이스 접근 (드라이버네임 string, DB 접속정보 string, maxopenconns int, maxidleconns int)
 // ex) dataframeutil.Open("mysql", "id:pw@tcp(127.0.0.1:3306)/TESTDB", 10, 5)
-func Open(driverName string, dataSourceName string, MaxOpenConns int, MaxIdleConns int) (*sql.DB, error) {
+func Open(driverName string, dataSourceName string, MaxOpenConns int, MaxIdleConns int) (*sql.DB, error){
 	db, err := sql.Open(driverName, dataSourceName)
 	CheckError(err)
 
@@ -26,12 +26,12 @@ func Open(driverName string, dataSourceName string, MaxOpenConns int, MaxIdleCon
 }
 
 // Close Open한 DB 닫기
-func Close(db *sql.DB) {
+func Close(db *sql.DB){
 	db.Close()
 }
 
 // Exec sql.Exec와 동일
-func Exec(query string, args ...interface{}) (sql.Result, error) {
+func Exec(query string, args ...interface{}) (sql.Result, error){
 	Result, err := db.Exec(query, args)
 	CheckError(err)
 
@@ -58,6 +58,7 @@ func LoadCSV(location string, isColHeader bool, delimiter rune) dataframe.DataFr
 	return csv
 }
 
+// SaveCSV 데이터프레임을 csv파일로 저장 (데이터프레임 df, 저장위치 string)
 func SaveCSV(df dataframe.DataFrame, location string) {
 	f, err := os.Create(location)
 	CheckError(err)
@@ -65,7 +66,8 @@ func SaveCSV(df dataframe.DataFrame, location string) {
 	df.WriteCSV(f)
 }
 
-// ReplaceElem 데이터프레임의 특정 값들을 원하는 값으로 변경(데이터프레임 df, 이전문자 string, 바꿀문자 string) 리턴타입 DataFrame
+// ReplaceElem 데이터프레임의 특정 값들을 원하는 값으로 변경(데이터프레임 df, 이전문자 interface{}, 바꿀문자 interface{}) 리턴타입 DataFrame
+// 바꿀 값이 속한 컬럼의 타입과 new의 타입이 동일해야 변경 가능
 func ReplaceElem(df dataframe.DataFrame, old interface{}, new interface{}) dataframe.DataFrame {
 	for c := 0; c < df.Ncol(); c++ {
 		for r := 0; r < df.Nrow(); r++ {
